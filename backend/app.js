1ff426d5ee6780335b77c9d31d8f84cdbfb4a775
Elisa-Beltramone <elibeltramone@gmail.com>
2026-06-27 15:21:42 +0200

    modification on express

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "../frontend")));
+
 app.get("/", (req, res) => {
   res.send("API running");
 });
